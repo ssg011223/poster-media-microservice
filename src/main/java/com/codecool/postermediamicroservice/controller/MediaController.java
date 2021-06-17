f7548241bf732634a4beae7c9ddf083eae46c0f4
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-17 14:28:40 +0200

    add: post endpoint for post media and person media

--- src/main/java/com/codecool/postermediamicroservice/controller/MediaController.java
+++ src/main/java/com/codecool/postermediamicroservice/controller/MediaController.java
@@ -1,11 +1,18 @@
 package com.codecool.postermediamicroservice.controller;
 
+import com.codecool.postermediamicroservice.model.Media;
+import com.codecool.postermediamicroservice.model.MediaTypeEnum;
+import com.codecool.postermediamicroservice.model.PersonMedia;
+import com.codecool.postermediamicroservice.repository.MediaRepository;
+import com.codecool.postermediamicroservice.repository.PersonMediaRepository;
 import com.codecool.postermediamicroservice.service.MediaService;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +23,10 @@ import java.io.InputStream;
 public class MediaController {
     @Autowired
     private MediaService mediaService;
+    @Autowired
+    private MediaRepository mediaRepository;
+    @Autowired
+    private PersonMediaRepository personMediaRepository;
 
     @GetMapping(value = "/{id}", produces = {MediaType.IMAGE_GIF_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
     public @ResponseBody byte[] getImage(@PathVariable String id) {
@@ -24,4 +35,34 @@ public class MediaController {
 
     @GetMapping(value = "/default-image", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody byte[] defaultImage() { return mediaService.getDefaultImage(); }
+
+    @PostMapping("/person/{id}")
+    public long postPersonMedia(@PathVariable String id,
+                                          @RequestParam("file") MultipartFile file) {
+        String route = mediaService.submit(file);
+        PersonMedia personMedia = PersonMedia.builder()
+                .personId(Long.parseLong(id))
+                .mediaRoute(route)
+                .mediaType(MediaTypeEnum.IMAGE)
+                .build();
+
+        personMediaRepository.save(personMedia);
+
+        return personMedia.getId();
+    }
+
+    @PostMapping("/post/{id}")
+    public long postPostMedia(@PathVariable String id,
+                                @RequestParam("file") MultipartFile file) {
+        String route = mediaService.submit(file);
+        Media media = Media.builder()
+                .postId(Long.parseLong(id))
+                .mediaRoute(route)
+                .mediaType(MediaTypeEnum.IMAGE)
+                .build();
+
+        mediaRepository.save(media);
+
+        return media.getId();
+    }
 }
